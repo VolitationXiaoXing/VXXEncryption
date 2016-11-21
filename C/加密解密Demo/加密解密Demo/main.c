0cb4d6c89e25d59e34f63776b3c0b55da3e9bcda
VolitationXiaoXing <752061676@qq.com>
2016-11-21 09:59:42 +0800

    删除C语言项目

--- C/加密解密Demo/加密解密Demo/main.c
+++ C/加密解密Demo/加密解密Demo/main.c
@@ -1,15 +1,15 @@
 //
-//  main.cpp
-//  TestDemo
+//  main.c
+//  加密解密Demo
 //
-//  Created by junge on 16/11/18.
+//  Created by junge on 16/11/21.
 //  Copyright © 2016年 VolitationXiaoXing. All rights reserved.
 //
 
-#include <iostream>
+#include <stdio.h>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    printf("Hello, World!\n");
     return 0;
 }
