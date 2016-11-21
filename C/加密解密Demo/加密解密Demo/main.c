306279ba3b4ef5d3a79c76f49c2692695617a304
VolitationXiaoXing <752061676@qq.com>
2016-11-21 10:01:37 +0800

    C语言加密

--- C/加密解密Demo/加密解密Demo/main.c
+++ C/加密解密Demo/加密解密Demo/main.c
@@ -2,14 +2,13 @@
 //  main.c
 //  加密解密Demo
 //
-//  Created by junge on 16/11/21.
+//  Created by VolitationXiaoXing on 16/11/21.
 //  Copyright © 2016年 VolitationXiaoXing. All rights reserved.
 //
 
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
     printf("Hello, World!\n");
     return 0;
 }
