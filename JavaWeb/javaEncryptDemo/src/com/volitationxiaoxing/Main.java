df10af665b4503b260692179b353b71a22f0351c
VolitationXiaoXing <752061676@qq.com>
2016-11-21 14:58:55 +0800

    更新java项目与ios同步

--- JavaWeb/javaEncryptDemo/src/com/volitationxiaoxing/Main.java
+++ JavaWeb/javaEncryptDemo/src/com/volitationxiaoxing/Main.java
@@ -1,18 +1,36 @@
 package com.volitationxiaoxing;
 
-import com.volitationxiaoxing.VXXEncryption.DESUtil;
+import com.volitationxiaoxing.VXXEncryption.VXXEncryptTools;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        String key = "1234567123456789";
+        String content = "1234abcd你好世界";
+        String iv = "1234567890123456";
 
-        DESUtil des = new DESUtil();
-        String s = des.getEnc("1234abcd你好世界");
+        try {
 
-
-
-        System.out.println(s);
+            System.out.println("===========================");
+            System.out.println("明文: "+content);
+            String encryptContent = VXXEncryptTools.encryptAES(content,key,iv);
+            System.out.println("===========================");
+            System.out.println("AES-128: "+encryptContent);
+            System.out.println("===========================");
+            System.out.println("AES-128: "+VXXEncryptTools.decryptAES(encryptContent,key,iv));
+            System.out.println("===========================");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- JavaWeb/javaEncryptDemo/src/com/volitationxiaoxing/VXXEncryption/VXXEncryptTools.java
+++ JavaWeb/javaEncryptDemo/src/com/volitationxiaoxing/VXXEncryption/VXXEncryptTools.java
@@ -1,15 +1,73 @@
 package com.volitationxiaoxing.VXXEncryption;
 
+
+
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.SecretKeyFactory;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import static javax.crypto.Cipher.*;
 
 /**
- * Created by junge on 16/11/17.
+ * Created by VolitationXiaoXing on 16/11/17.
  */
 public class VXXEncryptTools {
 
 
+    public static String encryptAES(String content, String key, String iv)
+            throws InvalidKeyException, NoSuchAlgorithmException,
+            NoSuchPaddingException, UnsupportedEncodingException,
+            InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+
+        byte[] byteContent = content.getBytes("UTF-8");
+
+        // 注意，为了能与 iOS 统一
+        // 这里的 key 不可以使用 KeyGenerator、SecureRandom、SecretKey 生成
+        byte[] enCodeFormat = key.getBytes();
+        SecretKeySpec secretKeySpec = new SecretKeySpec(enCodeFormat, "AES");
+
+        byte[] initParam = iv.getBytes();
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(initParam);
+
+        // 指定加密的算法、工作模式和填充方式
+        Cipher cipher = getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+
+        byte[] encryptedBytes = cipher.doFinal(byteContent);
+
+        // 同样对加密后数据进行 base64 编码
+        Base64.Encoder encoder = Base64.getEncoder();
+        return encoder.encodeToString(encryptedBytes);
+    }
 
 
+    public static String decryptAES(String content, String key,String iv) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
+
+        // base64 解码
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] encryptedBytes = decoder.decode(content);
+
+        byte[] enCodeFormat = key.getBytes();
+        SecretKeySpec secretKey = new SecretKeySpec(enCodeFormat, "AES");
+
+        byte[] initParam = iv.getBytes();
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(initParam);
+
+        Cipher cipher = getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, ivParameterSpec);
+
+        byte[] result = cipher.doFinal(encryptedBytes);
+
+        return new String(result, "UTF-8");
+    }
 
 }
